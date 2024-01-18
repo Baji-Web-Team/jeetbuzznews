c176fc438f00d681807843cb815385abd960a857
baji-web <web.team@auroramy.com>
2024-01-18 09:14:09 +0800

    test update

--- app/page.js
+++ app/page.js
@@ -7,6 +7,7 @@ import Videos from "./homesectioncomponents/Videos";
 export default function Home() {
   return (
     <main className="container flex  md:flex-col">
+      <h1>test update</h1>
       <Hero />
       <Predictions />
       <Videos />
