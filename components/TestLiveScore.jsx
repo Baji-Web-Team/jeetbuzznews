b43fa1bf670933fe33c24b2003d6af807e1237c0
Kashea <camrynchelsea01@gmail.com>
2024-02-27 15:53:10 +0800

    modified the test live score widget

--- components/TestLiveScore.jsx
+++ components/TestLiveScore.jsx
@@ -20,12 +20,11 @@ const SliderWidget = () => {
     }, 1000)
   }, [])
 
-  console.log({isLightMode})
-
   useEffect(() => {
     // Execute the script to initialize the widget
     const script = document.createElement('script');
-    script.innerHTML = `
+
+    const dark = `
       Entity_sport.push({
         code: "4654436544",
         field: "entity_cricket",
@@ -37,20 +36,40 @@ const SliderWidget = () => {
         where_to: "whereUwantToPutOnlyIdslider_widget",
         base_path: "https://jeetbuzznews.vercel.app/matches",
         links: "1",
-        color_type: ${!isLightMode ? "dark" : "light"},
+        color_type: "dark",
         choosed_color: "",
         choosed_preset: "",
       });
-    `;
-    document.getElementById('whereUwantToPutOnlyIdslider_widget')?.appendChild(script);
+    `
+
+    const light = `
+      Entity_sport.push({
+        code: "4654436544",
+        field: "entity_cricket",
+        widget_type: "content_type",
+        widget: "slider_widget",
+        id: "73748",
+        more_one: "",
+        widget_size: "large",
+        where_to: "whereUwantToPutOnlyIdslider_widget",
+        base_path: "https://jeetbuzznews.vercel.app/matches",
+        links: "1",
+        color_type: "light",
+        choosed_color: "",
+        choosed_preset: "",
+      });
+    `
+
+    script.innerHTML = isLightMode ? light : dark ;
+    document.getElementById('entity_widget').appendChild(script);
 
     // Clean up function to remove the script when the component unmounts
-    return () => {
-      document.getElementById('whereUwantToPutOnlyIdslider_widget').removeChild(script);
-    };
+    // return () => {
+    //   document.getElementById('whereUwantToPutOnlyIdslider_widget').removeChild(script);
+    // };
   }, []); // Empty dependency array ensures this effect runs only once
 
-  return <div id="whereUwantToPutOnlyIdslider_widget"></div>;
+  return <div id="entity_widget" />;
 };
 
 export default SliderWidget;
