c12487e9c88f18c964d0292aaa3fcf2d9e56e1ca
Kashea <camrynchelsea01@gmail.com>
2024-02-28 13:41:47 +0800

    update

--- app/livescore/page.jsx
+++ app/livescore/page.jsx
@@ -6,8 +6,10 @@ const page = () => {
   return (
     <main className='flex flex-col items-center min-h-screen'>
       <div className='container flex flex-col items-center xl:gap-x-12 px-6' >
-        <MatchDetails />
-        <AddDetails />
+        <div className='flex xl:flex-row md:flex-col flex-col items-center xl:gap-x-12 p-6'>
+          <MatchDetails />
+          <AddDetails />
+        </div>
       </div>
     </main>
   )
