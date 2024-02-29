d46e65f581b7b5031785d3e3bddc56fadc55c23e
Kashea <camrynchelsea01@gmail.com>
2024-02-29 17:03:49 +0800

    update dynamic page blog

--- app/blog/[Slug]/page.jsx
+++ app/blog/[Slug]/page.jsx
@@ -3,15 +3,15 @@ import BlogSection from "@/components/dyna66";
 import Scores from "@/components/SideScore";
 import WideAds from "@/components/WideAds";
 
-
-const Page = () => {
+const Page = ({ params }) => {
   const pageTitle = "Blogs"; // Set the title dynamically based on the page
 
   return (
     <main className="flex flex-col items-center min-h-screen">
       <div className="container flex flex-col items-center mx-auto mt-12">
         <div className="flex xl:flex-row md:flex-col flex-col items-center xl:gap-x-12 p-6">
-          <BlogSection/>
+          {/* Pass params to BlogSection component */}
+          <BlogSection params={params} />
           <Scores />
         </div>
         <div className="w-full items-center p6">
--- components/livescore/section33.jsx
+++ components/livescore/section33.jsx
@@ -4,6 +4,7 @@ const section33 = () => {
     return (
         <div className='basis-1/3 flex flex-col'>
             <div className='flex flex-col gap-6 items-start justify-start shrink-0 relative'>
+
                 
                 <img
                     className="self-stretch shrink-0 h-[300px] relative object-cover"
