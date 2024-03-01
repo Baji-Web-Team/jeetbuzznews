0de79b353ecc9a90e546e375864f23c0aa1cc160
Kashea <camrynchelsea01@gmail.com>
2024-03-01 16:14:08 +0800

    update

--- app/livescore/[Slug]/page.jsx
+++ app/livescore/[Slug]/page.jsx
@@ -1,8 +1,17 @@
 import React from 'react'
+import MatchDetails from '@/components/livescore/section66'
+import AddDetails from '@/components/livescore/section33'
 
-const page = ({params}) => {
+const page = () => {
   return (
-    <div>{params.match}</div>
+    <main className='flex flex-col items-center min-h-screen'>
+      <div className='container flex flex-col items-center xl:gap-x-12 px-6' >
+        <div className='flex xl:flex-row md:flex-col flex-col items-center xl:gap-x-12 p-6'>
+          <MatchDetails />
+          <AddDetails />
+        </div>
+      </div>
+    </main>
   )
 }
 
