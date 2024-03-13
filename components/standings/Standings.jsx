ce599f159eaf0fa0cb53fcacae7f5b1987bad777
baji-web <camrynchelsea01@gmail.com>
2024-03-13 12:28:34 +0800

    update

--- components/standings/Standings.jsx
+++ components/standings/Standings.jsx
@@ -35,7 +35,7 @@ const TeamStandings = () => {
       widget: "competetion_standings",
       id: "128410",
       more_one: "batting_most_runs",
-      widget_size: "small",
+      widget_size: "medium",
       where_to: "whereUwantToPutOnlyIdcompetetion_standings",
       base_path: "",
       links: "0",
