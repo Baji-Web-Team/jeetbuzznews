cd33f45b04ded2305d39a421747b78e56e2ef02a
baji-web <camrynchelsea01@gmail.com>
2024-03-13 17:17:38 +0800

    update

--- app/livescore/page.jsx
+++ app/livescore/page.jsx
@@ -5,8 +5,8 @@ import AddDetails from '@/components/SideScore'
 const page = () => {
   return (
     <main className='flex flex-col items-center min-h-screen'>
-      <div className='container flex flex-col items-center xl:gap-x-12 px-6' >
-        <div className='flex xl:flex-row md:flex-col flex-col items-center xl:gap-x-12 p-6'>
+      <div className='container flex flex-col  xl:gap-x-12 px-6' >
+        <div className='flex xl:flex-row md:flex-col flex-col  xl:gap-x-12 p-6'>
           <MatchDetails />
           <AddDetails />
         </div>
