913fba85aa165f7a02cdfe68a3bda9c3773409b3
baji-web <camrynchelsea01@gmail.com>
2024-03-13 12:21:48 +0800

    update

--- app/blog/page.jsx
+++ app/blog/page.jsx
@@ -9,7 +9,7 @@ const Page = () => {
 
   return (
     <main className="flex flex-col items-center min-h-screen">
-      <div className="container flex flex-col items-center mx-auto mt-12">
+      <div className="container flex flex-col  mx-auto mt-12">
         <div className="flex xl:flex-row md:flex-col flex-col items-center xl:gap-x-12 p-6">
           <BlogSection singleData={blogsSingle} allData={blogsAll} title={pageTitle} />
           <Scores />
