9dde08aeb0f165dc1d11e4d9f9d20b119484ede9
baji-web <camrynchelsea01@gmail.com>
2024-03-13 12:15:35 +0800

    update

--- components/Blogs66.jsx
+++ components/Blogs66.jsx
@@ -3,7 +3,7 @@ import Cards02 from "@/components/cards02";
 
 const BlogSection = ({ singleData, allData, title }) => {
   return (
-    <div className="lg:basis-2/3 flex flex-col top-0">
+    <div className="lg:basis-2/3 flex flex-col top-0 shrink-0">
       <div className="w-full relative flex flex-col items-start justify-start gap-[12px] mb-6">
         <h2 className="relative leading-[32px] font-semibold text-3xl">
           {title}
