b0b3d0594c9e01208caa401b518ebe17621b2ce0
baji-web <camrynchelsea01@gmail.com>
2024-03-14 11:22:24 +0800

    investigating

--- app/livescore/[Slug]/page.jsx
+++ app/livescore/[Slug]/page.jsx
@@ -2,12 +2,14 @@ import React from 'react'
 import MatchDetails from '@/components/livescore/section66matches'
 import AddDetails from '@/components/SideScore'
 
-const page = () => {
+
+const page = ({ searchParams }) => {
+  
   return (
     <main className='flex flex-col items-center min-h-screen'>
       <div className='container flex flex-col  xl:gap-x-12 px-6' >
         <div className='flex xl:flex-row md:flex-col flex-col xl:gap-x-12 p-6'>
-          <MatchDetails />
+          <MatchDetails searchParams={searchParams}  />
           <AddDetails />
         </div>
       </div>
--- components/livescore/section66matches.jsx
+++ components/livescore/section66matches.jsx
@@ -1,8 +1,11 @@
+'use client'
+
 import React from 'react'
 import LiveMatch from '../matches/LiveMatch'
 
+const section66 = ({ searchParams }) => {
+  console.log({searchParams})
 
-const section66 = () => {
   return (
     <div className='!lg:basis-2/3 flex flex-col top-0 !overflow-hidden !object-contain'>
         <LiveMatch />
