13c479840096da4cbad65e153f50bb1739b5bb0d
baji-web <camrynchelsea01@gmail.com>
2024-03-19 08:48:28 +0800

    update

--- app/blog/[Slug]/page.jsx
+++ app/blog/[Slug]/page.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import PropTypes from "prop-types";
 import BlogSection from "@/components/dyna66";
 import Scores from "@/components/SideScore";
 import WideAds from "@/components/WideAds";
@@ -10,16 +11,21 @@ const Page = ({ params }) => {
     <main className="flex flex-col items-center min-h-screen">
       <div className="container flex flex-col items-center mx-auto mt-12">
         <div className="flex xl:flex-row md:flex-col flex-col items-center xl:gap-x-12 p-6">
-          {/* Pass params to BlogSection component */}
-          <BlogSection params={params} />
+          {/* Pass params to BlogSection component if params exist */}
+          {params && <BlogSection params={params} />}
           <Scores />
         </div>
-        <div className="w-full items-center p6">
+        <div className="w-full items-center p-6"> {/* Corrected typo here */}
           <WideAds />
         </div>
       </div>
     </main>
   );
+};
+
+// PropTypes for Page component
+Page.propTypes = {
+  params: PropTypes.object // Assuming params is an object, adjust as necessary
 };
 
 export default Page;
--- components/dyna66.jsx
+++ components/dyna66.jsx
@@ -1,14 +1,22 @@
 import React from "react";
+import PropTypes from "prop-types";
+
 const Dyna66 = ({ params }) => {
+  // Check if params and params.Slug exist before accessing
+  const title = params && params.Slug ? params.Slug : "Title";
+
   return (
     <div className="lg:basis-2/3 flex flex-col top-0">
-      <div
-        className="flex flex-col gap-6 items-start justify-start flex-1 relative "
-      >
-        <h1>{params.Slug}</h1>
+      <div className="flex flex-col gap-6 items-start justify-start flex-1 relative">
+        <h1>{title}</h1>
       </div>
     </div>
   );
+};
+
+// PropTypes for Dyna66 component
+Dyna66.propTypes = {
+  params: PropTypes.object // Assuming params is an object, adjust as necessary
 };
 
 export default Dyna66;
